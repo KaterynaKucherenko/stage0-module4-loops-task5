8f92f8f9dc1d41170790caf00483228e769260ba
KaterynaKucherenko <estorskaya@gmail.com>
2022-12-16 02:52:19 +0300

    done

--- src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
+++ src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
@@ -2,6 +2,19 @@ package school.mjc.stage0.loops.task5;
 
 public class Hourglass {
     public void printHourglassOfGivenSize(int height) {
-
+String res = "";
+	    String a = " ";
+	 for (int x=0; x<=height; x++){
+	     for( int y = 0; y<x; y++){
+	         System.out.print(" ");}
+	         for (int r=x; r<height; r++){
+	             System.out.print("8" + " ");}  
+	             System.out.println();}
+	             
+	             for(int x = height-1; x>=1; x--){
+	                 for (int y=1; y<x; y++){
+	                     System.out.print(" ");}
+	                     for (int r=x; r<=height; r++){
+	                         System.out.print("8" + " ");}System.out.println();}
     }
 }
--- src/main/java/school/mjc/stage0/loops/task5/Triangle.java
+++ src/main/java/school/mjc/stage0/loops/task5/Triangle.java
@@ -2,6 +2,13 @@ package school.mjc.stage0.loops.task5;
 
 public class Triangle {
     public void printTriangle(int cathetusLength) {
+String res = "";
+	    String a = " ";
+	 for (int x=0; x<=cathetusLength; x++){
+	     for( int y = 0; y<x; y++){
+	         System.out.print("8");}
+	          
+	             System.out.println();}
 
     }
 }
